3b3ff2da7b56544167be3f29983addf181c4cb31
BrianRyan94 <brian.ryan@ucdconnect.ie>
2022-01-19 22:43:18 +0000

    Initial commit and utils package

---
+++ ftxhelperpy/utils/tests/test_connect.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import unittest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import connect
+
+class TestConnectMethods(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.connector = connect.Connector(os.getenv("FTX_ENDPOINT"))
+
+    def setUp(self) -> None:
+        self.connector = TestConnectMethods.connector
+
+    def test_auth_get_request_is_successful(self):
+        response = self.connector.auth_get_request("subaccounts")
+        is_success = response.json()['success']
+        self.assertTrue(is_success)
+
+    def test_auth_post_request_is_successful(self):
+        response = self.connector.auth_post_request("subaccounts", {"nickname": "test_sub_account"})
+        is_success = response.json()['success']
+        self.assertTrue(is_success)
+
+    def test_auth_delete_request_is_successful(self):
+        self.connector.auth_post_request("subaccounts", {"nickname": "test_sub_account"})
+        response = self.connector.auth_delete_request("subaccounts", {"nickname": "test_sub_account"})
+        is_success = response.json()['success']
+        self.assertTrue(is_success)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        #ensures that the test sub account that was made is deleted
+        cls.connector.auth_delete_request("subaccounts", {"nickname": "test_sub_account"})
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+

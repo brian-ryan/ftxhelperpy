f7645b50e2b8ae7563efec773a4cf26a0f5a46d0
BrianRyan94 <brian.ryan@ucdconnect.ie>
2022-01-24 23:17:13 +0000

    Add method for retrieving historical trades

--- ftxhelperpy/mktdata/prices.py
+++ ftxhelperpy/mktdata/prices.py
@@ -44,6 +44,22 @@ class HistDataFetcher:
         rates_df['time'] = rates_df['time'].apply(lambda x: parser.parse(x))
         return rates_df
 
+    def _format_trades(self, response: dict) -> pd.DataFrame:
+        """Converts the response from a historical trades request to a pandas dataframe
+            Args:
+                response: The json data of a response object from a historical funding rates request
+
+            Returns:
+                A pandas dataframe of the trade data"""
+
+        trades_dict = response['result']
+        if len(trades_dict)==0:
+            return pd.DataFrame
+
+        trades_df = pd.DataFrame.from_dict(trades_dict).sort_values(by='time', ascending=True).reset_index(drop=True)
+        trades_df['time'] = trades_df['time'].apply(lambda x: parser.parse(x))
+        return trades_df
+
     def get_historical_prices(self, symbol: str, start_time: datetime,
                               end_time: datetime, resolution: int = 60) -> pd.DataFrame:
         """Retrieves the historical prices (candle format) for a symbols
@@ -56,7 +72,7 @@ class HistDataFetcher:
                 e.g. resolution = 300 means 5 minute candles
 
             Returns:
-                A pandas dataframe with columns for the st"""
+                A pandas dataframe of historical prices"""
 
         endpoint = "markets/{0}/candles".format(symbol)
         query_params = {
@@ -72,7 +88,7 @@ class HistDataFetcher:
         return self._format_prices(response)
 
     def get_historical_rates(self, symbol: str, start_time: datetime, end_time: datetime) -> pd.DataFrame:
-        """Retrieves the historical prices (candle format) for a symbols
+        """Retrieves the historical prices (candle format) for a symbol
 
             Args:
                 symbol: The symbol of the instrument. e.g. BTC-PERP
@@ -101,3 +117,31 @@ class HistDataFetcher:
             raise Exception(response['error'])
 
         return self._format_rates(response)
+
+    def get_historical_trades(self, symbol: str, start_time: datetime, end_time: datetime) -> pd.DataFrame:
+        """Retrieves the historical trades for a given symbol
+
+            Args:
+                symbol: The symbol of the instrument. e.g. BTC-PERP
+                start_time: Start of the interval of time to retrieve trades
+                end_time: End of the interval of time to retrieve trades
+
+            Returns:
+                A pandas dataframe with columns for the trade id,
+                price, size, side, liquidation (boolean if the trade involved
+                a liquidation order), and time
+        """
+
+        endpoint = "/markets/{0}/trades".format(symbol)
+        query_params = {
+            'market_name': symbol,
+            'start_time': datetime.timestamp(start_time),
+            'end_time':datetime.timestamp(end_time)
+        }
+
+        response = self.connector.auth_get_request(endpoint, query_params).json()
+
+        if response['success']==False:
+            raise Exception(response['error'])
+
+        return self._format_trades(response)

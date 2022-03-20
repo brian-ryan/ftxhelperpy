5ddda9020bc6ecc522b6cb8e33c633fb994edfc5
BrianRyan94 <brian.ryan@ucdconnect.ie>
2022-03-20 22:08:40 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name = 'ftxhelperpy',
-    version = '0.1.9',
+    version = '0.1.0',
     description = 'Python wrapper for interacting with the FTX api',
     url = 'https://github.com/BrianRyan94/ftxhelperpy',
     packages = setuptools.find_packages(),

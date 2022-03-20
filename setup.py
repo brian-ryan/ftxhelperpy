8dade12dcd2281af05e611e5a23eac9d9714b12b
BrianRyan94 <brian.ryan@ucdconnect.ie>
2022-03-20 22:08:23 +0000

    Add setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'ftxhelperpy',
+    version = '0.1.9',
+    description = 'Python wrapper for interacting with the FTX api',
+    url = 'https://github.com/BrianRyan94/ftxhelperpy',
+    packages = setuptools.find_packages(),
+    license = 'MIT',
+    install_requires = [line.strip() for line in open("requirements.txt", "r").readlines()]
+)
